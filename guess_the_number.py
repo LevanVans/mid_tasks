ea1687df45c22c27fd0c55997fca1860e1ed16b6
Levan <118633835+LevanVans@users.noreply.github.com>
2024-03-02 22:02:06 +0400

    Update guess_the_number.py
    Uploading final version which was presented

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,49 +1,58 @@
 import random
 
-first_number =  1
-last_number = 10
+# Random numbers to verify range 
 
-random_number = random.randint(first_number, last_number)
+first_random_number =  1
+last_random_number = 20
 
+# Random number initialization 
+
+random_number = random.randint(first_random_number, last_random_number)
+
+
+# Attemps count 
 attemps = 0
 
+
+# Main Code
 while True:
     
     try:
-        guess_number = int(input(f"Guess The Integer number from range {first_number} - {last_number} : \n "))
+        your_number = int(input(f"Guess The Integer number from range {first_random_number} - {last_random_number} : \n "))
     
-    except:
+    except(ValueError):
         
-        print("Wrong Input type Try Again (This will be added to your attemps)")
+        print("Wrong Input type Try Again (This will not be added to your attemps count)")
         continue
     
     
-    if guess_number < first_number or guess_number > last_number:
+    if your_number < first_random_number or your_number > last_random_number:
         
-        print("Number out of Range, Try Again ")
+        print("Number out of Range, Try Again (This will not be added to your attemps count) ")
         continue
     
     attemps += 1
     
-    if guess_number == random_number : 
+    if your_number == random_number : 
         
         print(f"Good Job , You took Only {attemps} attemps to guess number : {random_number} \n")
         
         attemps = 0
     
-    elif guess_number < random_number :
+    elif your_number < random_number :
         
-        print(f"Random number is Higher , your attemps is {attemps} \n")
-        
-        continue
-        
-    elif guess_number > random_number:
-        
-        print(f"Random number is Lower , your attemps is {attemps} \n")
+        print(f"\n Choose Higher , your attemps count is {attemps} \n")
         
         continue
         
+    elif your_number > random_number:
         
+        print(f" \n Choose Lower , your attemps count is {attemps} \n")
+        
+        continue
+        
+    # Continue or finish program      
+    
     question = input("Press Enter Play Again or Input any symbol to exit :  ")
     
     if question == "":
